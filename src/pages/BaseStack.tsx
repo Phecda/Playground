58f3c3e4047cf46aa28f227eca80210f785f25b1
Phecda <szp12345141@gmail.com>
2023-03-31 01:29:09 +0800

    feat: themes for navigation

--- src/pages/BaseStack.tsx
+++ src/pages/BaseStack.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeTabs from './HomeTabs';
-import { NavigationContainer } from '@react-navigation/native';
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from '@react-navigation/native';
+import { useColorScheme } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 export default function BaseStack() {
+  const colorScheme = useColorScheme();
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack.Navigator>
         <Stack.Screen
           name="Home"
