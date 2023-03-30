359698b2f206a311279e5f19a2fb23fee9e2172d
Phecda <szp12345141@gmail.com>
2023-03-31 00:57:31 +0800

    feat: add react-native-vector-icons

--- src/pages/HomeTabs.tsx
+++ src/pages/HomeTabs.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import Welcome from './Welcome';
 import PlaceholderPage from './PlaceholderPage';
 
@@ -7,7 +8,24 @@ const Tab = createBottomTabNavigator();
 
 export default function HomeTabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => {
+        return {
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = 'home';
+            if (route.name === 'Placeholder') {
+              iconName = 'list';
+            } else if (route.name === 'Welcome') {
+              if (focused) {
+                iconName = 'home';
+              } else {
+                iconName = 'home-outline';
+              }
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        };
+      }}>
       <Tab.Screen name="Welcome" component={Welcome} />
       <Tab.Screen name="Placeholder" component={PlaceholderPage} />
     </Tab.Navigator>
